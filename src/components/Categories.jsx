eb9b3101ece9bf53e291a28ada044e12cd62236c
Salman <salmanajani0426@gmail.com>
2023-04-05 17:41:33 +0530

    all blogs display part done

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 
 const Categories = () => {
   return (
-    <div className="categories">
-      <Link to="/?category=art">Art</Link>
-      <Link to="/?category=science">Science</Link>
-      <Link to="/?category=technology">Technology</Link>
-      <Link to="/?category=cinema">Cinema</Link>
-      <Link to="/?category=design">Design</Link>
-      <Link to="/?category=food">Food</Link>
+    <div className="category-container">
+      <h2>Categories</h2>
+      <div className="categories">
+        <Link to="/">All</Link>
+        <Link to="/?category=art">Art</Link>
+        <Link to="/?category=science">Science</Link>
+        <Link to="/?category=technology">Technology</Link>
+        <Link to="/?category=cinema">Cinema</Link>
+        <Link to="/?category=design">Design</Link>
+        <Link to="/?category=food">Food</Link>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Categories from "../components/Categories";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const blogs = [
-  {
-    id: 1,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 2,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 3,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 4,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-];
+// const blogs = [
+//   {
+//     id: 1,
+//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+//     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+//   {
+//     id: 2,
+//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+//     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+//   {
+//     id: 3,
+//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+//     img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+//   {
+//     id: 4,
+//     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+//     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+// ];
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const category = useLocation().search;
+  // console.log(category);
+
+  const fetchBlogs = async () => {
+    try {
+      const res = await axios.get(`http://localhost:4000/blogs${category}`);
+      setBlogs(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [category]);
+
   return (
     <div>
       <Categories />
@@ -40,7 +60,7 @@ const Home = () => {
             </div>
             <div className="content">
               <Link to={`/blogs/${blog.id}`}>
-                <h1>{blog.title}</h1>
+                <h2>{blog.title}</h2>
               </Link>
               <p>{blog.desc}</p>
               <Link to={`/blogs/${blog.id}`}>
